215012e98253fb70b5e50bc7742211636b0bb024
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-07-24 15:42:21 +0200

    remove counter test case

--- dag_test.ts
+++ dag_test.ts
@@ -1,13 +1,13 @@
 import { assert } from "https://deno.land/std@0.192.0/_util/asserts.ts";
 import { dag } from "./dag.ts";
-import { isObject } from "./typeof.ts";
 
 Deno.test("dependency graph", async () => {
+  type Build = typeof build;
   const build = { name: "build command", success: true };
 
-  const isBuild = (value: unknown): value is typeof build => value === build;
+  const isBuild = (value: unknown): value is Build => value === build;
   const runBuild = () =>
-    new Promise<{ success: boolean }>((resolve) => {
+    new Promise<Build>((resolve) => {
       setTimeout(resolve, 1000, build);
     });
 
@@ -22,71 +22,17 @@ Deno.test("dependency graph", async () => {
     vue: { lib: "lib", core: "core" },
     angular: { debugger: "debugger", core: "core" },
   };
-  const dependencyTreeKeys = Object.keys(dependencyTree);
 
   const graph = dag(dependencyTree);
 
-  const result = await Promise.all(
-    dependencyTreeKeys.map(async (dependencyName) => {
-      const graphItem = await graph.get(dependencyName);
-
-      return (isBuild(graphItem) ? graphItem : await runBuild());
-    }),
-  );
-
   assert(
-    result.every(isBuild),
+    (await Promise.all(
+      Object.keys(dependencyTree).map(async (dependencyName) => {
+        const graphItem = await graph.get(dependencyName);
+
+        return (isBuild(graphItem) ? graphItem : await runBuild());
+      }),
+    )).every(isBuild),
     "each result item should be build command result",
   );
-});
-
-Deno.test("Counter", async () => {
-  const sum = (a: unknown, b: unknown) =>
-    typeof a === "number" && typeof b === "number" ? a + b : 0;
-
-  const deepValues = <Obj extends Record<PropertyKey, unknown>>(
-    obj: Obj,
-  ): unknown[] =>
-    Object.values(obj).flatMap((value) =>
-      isObject(value) ? deepValues(value) : value
-    );
-
-  const counterObj = {
-    "1": Promise.resolve(1),
-    "2": {
-      value1: "1",
-      value2: "1",
-    },
-    "3": {
-      value1: "1",
-      value2: "2",
-    },
-    "4": {
-      value1: "2",
-      value2: "2",
-    },
-    "5": {
-      value1: "2",
-      value2: "3",
-    },
-  } as const;
-  const counterObjKeys = Object.keys(counterObj);
-
-  const graph = dag(counterObj);
-
-  const result = await Object.keys(counterObj).reduce(
-    async (collector, graphKey) => {
-      const graphValue = await graph.get(graphKey);
-
-      const nextValue = isObject(graphValue)
-        ? deepValues(graphValue).reduce<number>(sum, 0)
-        : graphValue;
-
-      return await collector + nextValue;
-    },
-    Promise.resolve(0),
-  );
-  const expectedResult = counterObjKeys.map(Number).reduce(sum, 0);
-
-  assert(result === expectedResult);
 });
